7c3d696eb32a6140429f5a147a67410663a352ae
Karol Żynda <k.zynda@il-pib.pl>
2020-06-08 23:10:12 +0200

    First controller version

---
+++ PlexWebHook/Controllers/PlexHook.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlexWebHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlexHook : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var result = new ActionResult<string>("ok12");
+
+            var body = string.Empty;
+            
+            using (var reader = new StreamReader(Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+
+            return result;
+        }
+    }
+}
